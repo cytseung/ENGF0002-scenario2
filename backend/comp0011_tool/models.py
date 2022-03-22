4b4c59bf65d44120de693b7f7d7259eb23556137
cyt <NA>
2022-03-22 11:22:27 +0000

    file_upload

--- backend/comp0011_tool/models.py
+++ backend/comp0011_tool/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Type(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     
--- backend/comp0011_tool/views.py
+++ backend/comp0011_tool/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -30,7 +31,57 @@ class FileUploadView(APIView):
     parser_classes = [MultiPartParser, FormParser,]
     # save questions here
     def post(self, request, format=None):
-        for line in request.data['file']:
-            s = line.decode().strip()
-            print(s)
-        return Response(request.data['file'])
+        file = request.data['file']
+        try:
+            numQ = int(file.readline().decode().strip())
+        except ValueError:
+            raise ValidationError({"number of questions must be an integer"})
+        else:
+            count = 0
+            for i in range(numQ):
+                q_type = file.readline().decode().strip()
+                if all(t.name != q_type for t in Type.objects.all()):
+                    raise ValidationError({f'{count} question(s) imported. {q_type} type not defined'.format(q_type=q_type, count=count)})
+                q_type_obj = Type.objects.get(name=q_type)
+                try:
+                    m1_m, m1_n =list(map(int, file.readline().decode().strip().split()))
+                    m2_m, m2_n =list(map(int, file.readline().decode().strip().split()))
+                    m1_values = list(map(int, file.readline().decode().strip().split()))
+                    m2_values = list(map(int ,file.readline().decode().strip().split()))
+                    if m1_m * m1_n != len(m1_values) or m2_m * m2_n != len(m2_values):
+                        raise ValidationError({f'{count} question(s) imported. Dimensions do not match with number of elements'.format(count=count)})
+                    m1 = []
+                    m2 = []
+                    for i in range(m1_m):
+                        row = []
+                        for j in range(m1_n):
+                            row.append(m1_values[m1_n*i+j])
+                        m1.append(row)
+                    for i in range(m2_m):
+                        row = []
+                        for j in range(m2_n):
+                            row.append(m2_values[m2_n*i+j])
+                        m2.append(row)
+                    ans_m, ans_n = list(map(int, file.readline().decode().strip().split()))
+                    ans_values = list(map(int, file.readline().decode().strip().split()))
+                    if ans_m * ans_n != len(ans_values):
+                        raise ValidationError({f'{count} question(s) imported. Dimensions do not match with number of elements'.format(count=count)})
+                    ans = []
+                    for i in range(ans_m):
+                        row = []
+                        for j in range(ans_n):
+                            row.append(ans_values[ans_n*i+j])
+                        ans.append(row)
+                    importedQ = ImportedMatrixQuestion(type=q_type_obj, matrix1=str(m1), matrix2=str(m2), answer=str(ans))
+                    importedQ.save()
+                    count += 1
+
+                except ValueError:
+                    raise ValidationError({f"{count} question(s) imported. Dimensions must be integers".format(count=count)})
+            if file.readline() != "":
+                raise ValidationError({f"{count} question(s) imported. You specifed only {numQ} but provided more than {numQ} question(s)".format(count=count,numQ=numQ)})
+            
+
+
+            
+        return Response({"Import Success. {count} question(s) imported.".format(count=count)})
